0b964775d7ef3f60d8447b9431c322ad09a8d6b3
edicson <edicson.garcia.luis@umontreal.ca>
2020-10-17 16:03:02 -0400

    Abstract contient la liste de mots de chaque article

--- Naive_Bayes.py
+++ Naive_Bayes.py
@@ -1,22 +1,17 @@
-
-
 import numpy as np
 import pandas as pd
 from scipy import sparse
 import string
 
 
-
 # Erase all the punctuation
 def pd_translate(df):
     df = df.replace(r'\n',' ', regex=True)
     punct = string.punctuation
-    transtab = str.maketrans(dict.fromkeys(punct, ''))
+    transtab = str.maketrans(dict.fromkeys(punct, ' '))
     return df.assign(
         Abstract='¿'.join(df['Abstract'].tolist()).translate(transtab).split('¿')
     )
-
-
 
 # Count the number of words in a list and erase the ones that are in the another list
 def count_and_erase_words(lst, common_list):
@@ -36,11 +31,11 @@ def count_and_erase_words(lst, common_list):
                 word_count.pop(item, None)
 
     # Calculate the probability for each word
-    nom_elements = len(lst)
-    for key in word_count:
-        word_count[key] = word_count[key] / nom_elements
-
-    return word_count
+    #nom_elements = len(lst)
+    #for key in word_count:
+    #    word_count[key] = word_count[key] / nom_elements
+    
+    return list(word_count.keys())
 
 
 def load_common_words(file):
@@ -53,7 +48,8 @@ def pre_traitement(df, common_words):
     # Remove the punctuation, blank spaces and put everything in lowercase
     df = df.assign(Abstract=pd_translate(df)['Abstract'].str.lower())
     df = df.assign(Abstract=df['Abstract'].str.strip())
-
+    df = df.replace(r'\s+',' ', regex=True)
+    
     # Split each phrase in words
     df = df.assign(Abstract=df['Abstract'].str.split(' '))
 
@@ -61,13 +57,13 @@ def pre_traitement(df, common_words):
     prob = []
     for row in df['Abstract']:
         prob.append(count_and_erase_words(row, common_words))
-    df['probability'] = prob
-
+    #df['probability'] = prob
+    df = df.assign(Abstract=prob)
+    
     return df
 
 
-data = pd.read_csv("train.csv")
+df= pd.read_csv("train.csv")
 common_words = load_common_words('common_english_words.txt')
-
-df = pre_traitement(data, common_words)
-print(df['Abstract'])
+unique_labels = df['Category'].unique()
+data = pre_traitement(df, common_words)
